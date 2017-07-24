01f71305be355440fcd3a17fc17a4e8feefbaa02
serhiistarozhuk <Therexxx94@gmail.com>
2017-07-24 15:04:03 +0300

    Update urls.py
    some fix

--- gallery/photostorage/photostorage/urls.py
+++ gallery/photostorage/photostorage/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from photogallery import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -25,4 +27,4 @@ urlpatterns = [
     url(r'^captcha/', include('captcha.urls')),
     url(r'^popular/$', views.gallery_popular, name='popular'),
     url(r'^allphoto/$', views.allphoto, name='allphoto'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
